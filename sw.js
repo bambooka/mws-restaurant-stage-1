9c5b332b41df8890b7fa5db3109296f6ac3be13f
OlaBooka <booka@yandex.ru>
2018-07-16 10:13:32 +0300

    Feat: Put into cache all

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
 const CACHE = 'network-or-cache-v1';
+
+// Put into cache
+self.addEventListener('fetch', (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => cache.addAll([
+                event.request
+            ])
+        ));
+});
