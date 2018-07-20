ef80635a0fd51f898b1e2be361f19c6cc7148dec
OlaBooka <booka@yandex.ru>
2018-07-20 14:25:33 +0300

    Style: add TODO for port and domain

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,9 @@ class DBHelper {
      * Change this to restaurants.json file location on your server.
      */
     static get DATABASE_URL() {
-        const port = 80; // Change this to your server port
-        return `http://restaurant-reviews.localhost:${port}/data/restaurants.json`;
+        const port = 80; // TODO: Change this to your server port
+        const domain = 'restaurant-reviews.localhost';  // TODO: Change this to your domain name
+        return `http://${domain}:${port}/data/restaurants.json`;
     }
 
     /**
