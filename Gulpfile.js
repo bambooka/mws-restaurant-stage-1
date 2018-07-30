d1a8d68baa184aeef02488d1b30897a6000bca9c
OlaBooka <booka@yandex.ru>
2018-07-31 02:59:48 +0300

    feat: Include gulp plugins

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,7 @@
 const gulp = require('gulp');
 
+var uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+
+const concat = require('gulp-concat');
+let minCSS = require('gulp-clean-css');
