2742b5cefbea7d3af3a038f96845fa63c7910102
OlaBooka <booka@yandex.ru>
2018-07-31 03:00:33 +0300

    feat: Use plugin browser sync

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,3 +5,11 @@ const browserSync = require('browser-sync').create();
 
 const concat = require('gulp-concat');
 let minCSS = require('gulp-clean-css');
+
+gulp.task('browserSync', function(){
+    browserSync({
+        server: {
+            baseDir: 'mws-restaurant-stage-1'
+        }
+    })
+});
