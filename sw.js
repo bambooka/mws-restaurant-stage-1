c5fcad32ef4e95f0d948d10ce36a506d9a6ddddc
OlaBooka <booka@yandex.ru>
2018-07-31 15:38:45 +0300

    FEAT: add detailed page html to cache

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
 const CACHE = 'cache';
 var toCache = [
+    'index.html',
+    'restaurant.html?id=1',
+    'restaurant.html?id=2',
+    'restaurant.html?id=3',
+    'restaurant.html?id=4',
+    'restaurant.html?id=5',
+    'restaurant.html?id=6',
+    'restaurant.html?id=7',
+    'restaurant.html?id=8',
+    'restaurant.html?id=9',
+    'restaurant.html?id=10',
     './css/beautify_detailed_view.css',
     './css/orientation_landscape.css',
     './data/restaurants.json',
