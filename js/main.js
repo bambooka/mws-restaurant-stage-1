47de06e7789c0998da4f996745b4677807a6ed44
bambooka <onceup0nAtime>
2020-06-07 14:59:16 +0300

    chore: change init coordinates

--- js/main.js
+++ js/main.js
@@ -73,8 +73,8 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  */
 window.initMap = () => {
     let loc = {
-        lat: 59.9390164,
-        lng: 30.3232096
+        lat: 59.9092911,
+        lng: 30.2813323
     };
     self.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
